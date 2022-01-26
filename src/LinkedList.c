384e15fc62b2914e3275007261a724f61c5a8211
Alex <alexpdev@protonmail.com>
2022-01-26 12:31:11 -0800

    LList Updates

---
+++ src/LinkedList.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+Node *newNode(void *data, int size)
+{
+    Node *node = (Node *)malloc(sizeof(Node) + size);
+    assert(node);
+    node->data = data;
+    node->size = size;
+    node->next = NULL;
+    return node;
+}
+
+Node *emptyNode()
+{
+    return (Node *)malloc(sizeof(Node));
+}
+
+void LLshow(Node *node)
+{
+    Node *current = node;
+    if(!node) return;
+    while (current != NULL){
+        current = current->next;
+        printf("%s\n", (char *)current->data);
+    }
+}
+
+void LL_append(LList *list, Node *next)
+{
+    Node *last_node = list->end;
+    last_node->next = next;
+}
+
+
+void LL_delete(LList *list, Node *node)
+{
+    Node *previous = top;
+    Node *current = top->next;
+    while(current != NULL){
+        if(current->data == data){
+            previous->next = current->next;
+            if(current == top)
+                top = current->next;
+            free(current);
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+}
+
+void LL_destroy(LList *list)
+{
+    Node *next;
+    Node *current = top;
+    while(current != NULL){
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    free(top);
+}
--- src/LinkedList.h
+++ src/LinkedList.h
@@ -11,75 +11,21 @@ SOFTWARE.
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-struct Node{
+typedef struct Node {
     void *data;
-    struct Node *next;
+    int size;
+    Node *next;
 } Node;
 
-Node *newNode(int data)
-{
-    Node *node = (Node *)malloc(sizeof(Node));
-    assert(node);
-    newNode->data = data;
-    newNode->next = NULL;
-     return newNode;
-}
-
-Node *emptyNode()
-{
-    return (Node *)malloc(sizeof(Node));
-}
-
-void show(Node *node)
-{
-    Node *current = node;
-    if(!node) return;
-    while (current != NULL){
-        current = current->next;
-        printf("%s\n", (char *)current->data);
-    }
-}
-
-void append(int data, Node *top)
-{
-    Node *current = NULL;
-    if(!top) {
-        top = newNode(data);
-        return;
-    }
-    current = node;
-    while (current->next != NULL){
-      current = current->next;
-    }
-    current->next = createnode(data);
-}
+typedef struct LList {
+    size_t length;
+    Node *start;
+    Node *end;
+} LList;
 
 
-void delete(int data, Node *top)
-{
-    Node *previous = top;
-    Node *current = top->next;
-    while(current != NULL){
-        if(current->data == data){
-            previous->next = current->next;
-            if(current == top)
-                top = current->next;
-            free(current);
-            return;
-        }
-        previous = current;
-        current = current->next;
-    }
-}
-
-void destroy(Node *top)
-{
-    Node *next;
-    Node *current = top;
-    while(current != NULL){
-        next = current->next;
-        free(current);
-        current = next;
-    }
-    free(top);
-}
+Node *newNode(void *data, int size);
+void LLshow(Node *node);
+void LL_append(LList *list, Node *next);
+void LL_delete(LList *list, Node *node);
+void LL_destroy(LList *list);

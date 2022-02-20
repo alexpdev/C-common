b70b2250db7cf1ce733bcae79c543d4933e5fea6
Alex <alexpdev@protonmail.com>
2022-02-19 21:20:58 -0800

    added_comparison

---
+++ Comparison/compare.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "CompString.h"
+
+int stringEqual(void *str1, void *str2)
+{
+    return strcmp((char *) str1, (char*) str2);
+}
+
+int stringComp(void *str1, void *str2)
+{
+    return stringEqual(str1, str2);
+}
+
+int pointerCmp(void *ptr1, void *ptr2)
+{
+    if (ptr1 == ptr2){
+        return 0;
+    }
+    return -1;
+}
+
+int intCmp(void *n1, void *n2)
+{
+    int i, j;
+    i = *n1;
+    j = *n2;
+    return i - j;
+}
---
+++ Comparison/compare.h
@@ -0,0 +1,9 @@
+#ifndef COMPARE_H
+#define COMPARE_H
+
+int stringEqual(void *string1, void *string2);
+int stringCmp(void *string1, void *string2);
+int pointerCmp(void *ptr1, void *ptr2);
+int intCmp(void *n1, void *n2);
+
+#endif
